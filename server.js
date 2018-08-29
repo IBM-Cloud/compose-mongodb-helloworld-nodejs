4137c3f41981d9ae0b2fe1044a852111f6c8b585
Dj Walker-Morgan <codepope@gmail.com>
2018-08-29 16:49:49 +0100

    Updated for LE Certs with SS support retained

--- server.js
+++ server.js
@@ -43,13 +43,15 @@ var cfenv = require('cfenv');
 // load local VCAP configuration  and service credentials
 var vcapLocal;
 try {
-  vcapLocal = require('./vcap-local.json');
-  console.log("Loaded local VCAP");
-} catch (e) { 
+    vcapLocal = require('./vcap-local.json');
+    console.log("Loaded local VCAP");
+} catch (e) {
     // console.log(e)
 }
 
-const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
+const appEnvOpts = vcapLocal ? {
+    vcap: vcapLocal
+} : {}
 
 const appEnv = cfenv.getAppEnv(appEnvOpts);
 
@@ -65,19 +67,17 @@ assert(!util.isUndefined(mongodb_services), "App must be bound to compose-for-mo
 // We now take the first bound MongoDB service and extract it's credentials object
 let credentials = mongodb_services[0].credentials;
 
-// Setting nothing in the options will assume no SSL
-let options = {};
-
-// Within the credentials, an entry ca_certificate_base64 contains the SSL pinning key
-// We convert that from a string into a Buffer entry in an array which we use when
-// connecting.
-let ca = [new Buffer(credentials.ca_certificate_base64, 'base64')];
-
-options = {
-  ssl: true,
-  sslValidate: true,
-  sslCA: ca
+// We always want to make a validated TLS/SSL connection
+let options = {
+    ssl: true,
+    sslValidate: true
 };
+
+// If there is a certificate available, use that, otherwise assume Lets Encrypt certifications.
+if (credentials.hasOwnProperty("ca_certificate_base64")) {
+    let ca = [new Buffer(credentials.ca_certificate_base64, 'base64')];
+    options.sslCA = ca;
+}
 
 // We want to extract the port to publish our app on
 let port = process.env.PORT || 8080;
@@ -90,56 +90,55 @@ let mongodb;
 // connect to that URI, and also pass a number of SSL settings to the
 // call. Among those SSL settings is the SSL CA, into which we pass the array
 // wrapped and now decoded ca_certificate_base64,
-MongoClient.connect(credentials.uri, options, function(err, db) {
-        // Here we handle the async response. This is a simple example and
-        // we're not going to inject the database connection into the
-        // middleware, just save it in a global variable, as long as there
-        // isn't an error.
-        if (err) {
-            console.log(err);
-        } else {
-            // Although we have a connection, it's to the "admin" database
-            // of MongoDB deployment. In this example, we want the
-            // "examples" database so what we do here is create that
-            // connection using the current connection.
-            mongodb = db.db("examples");
-        }
+MongoClient.connect(credentials.uri, options, function (err, db) {
+    // Here we handle the async response. This is a simple example and
+    // we're not going to inject the database connection into the
+    // middleware, just save it in a global variable, as long as there
+    // isn't an error.
+    if (err) {
+        console.log(err);
+    } else {
+        // Although we have a connection, it's to the "admin" database
+        // of MongoDB deployment. In this example, we want the
+        // "examples" database so what we do here is create that
+        // connection using the current connection.
+        mongodb = db.db("examples");
     }
-);
+});
 
 // Add a word to the database
 function addWord(word, definition) {
-  return new Promise(function(resolve, reject) {
-      mongodb.collection("words").insertOne({
-              word: word,
-              definition: definition
-          },
-          function(error, result) {
-              if (error) {
-                  reject(error);
-              } else {
-                  resolve(result);
-              }
-          }
-      );
-  });
+    return new Promise(function (resolve, reject) {
+        mongodb.collection("words").insertOne({
+                word: word,
+                definition: definition
+            },
+            function (error, result) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            }
+        );
+    });
 }
 
 // Get words from the database
 function getWords() {
-  return new Promise(function(resolve, reject) {
-      // we call on the connection to return us all the documents in the words collection.
-      mongodb
-          .collection("words")
-          .find()
-          .toArray(function(err, words) {
-              if (err) {
-                  reject(err);
-              } else {
-                  resolve(words);
-              }
-          });
-  });
+    return new Promise(function (resolve, reject) {
+        // we call on the connection to return us all the documents in the words collection.
+        mongodb
+            .collection("words")
+            .find()
+            .toArray(function (err, words) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(words);
+                }
+            });
+    });
 }
 
 // With the database going to be open as some point in the future, we can
@@ -148,12 +147,12 @@ app.use(express.static(__dirname + "/public"));
 
 // The user has clicked submit to add a word and definition to the database
 // Send the data to the addWord function and send a response if successful
-app.put("/words", function(request, response) {
+app.put("/words", function (request, response) {
     addWord(request.body.word, request.body.definition)
-        .then(function(resp) {
+        .then(function (resp) {
             response.send(resp);
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.log(err);
             response.status(500).send(err);
         });
@@ -161,18 +160,18 @@ app.put("/words", function(request, response) {
 
 // Read from the database when the page is loaded or after a word is successfully added
 // Use the getWords function to get a list of words and definitions from the database
-app.get("/words", function(request, response) {
+app.get("/words", function (request, response) {
     getWords()
-        .then(function(words) {
+        .then(function (words) {
             response.send(words);
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.log(err);
             response.status(500).send(err);
         });
 });
 
 // Listen for a connection.
-app.listen(port, function() {
+app.listen(port, function () {
     console.log("Server is listening on port " + port);
 });
